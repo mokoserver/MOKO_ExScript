9007e02c358392c9e835b678959d6976ccdf2b47
AlexNistuk <sasha.nistyuk@mail.ru>
2022-10-24 14:24:19 +0300

    Updated MOKO Clicker script

--- Example/Clicker Project/2 Сommands execution (выполнение команд).py
+++ Example/Clicker Project/2 Сommands execution (выполнение команд).py
@@ -1,5 +1,4 @@
 import MOKO
-from MOSC import stars
 import MCLK
 
 
@@ -19,7 +18,7 @@ MOKO.Program('tree', 'set', 'chosen = passed')
 #description: Decribe;command;;(описание;команды);
 #hesh The second command: to specify the;image path;;(указать путь;картинки)
 
-MCLK.PngPathCommand("C:/MOKO SE/Images/Desktop MOKO Long.png")
+MCLK.PngPath("C:/MOKO SE/Images/Desktop MOKO Long.png")
 
 MOKO.Program('tree', 'set', 'select = ' + 'The second command')
 MOKO.Program('tree', 'set', 'chosen = passed')
@@ -28,7 +27,7 @@ MOKO.Program('tree', 'set', 'chosen = passed')
 #Region Status (статус)
 #description: Decribe;command;;(описание;команды);
 #hesh The third command: to get the image;from the specified;path;(получить;картинку;по указанному;пути)
-png_file = MCLK.GetPngFileCommand()
+png_file = MCLK.GetPngFile()
 MOKO.Report("ClickerFile", 'set', 'picture', png_file)
 
 MOKO.Program('tree', 'set', 'select = ' + 'The third command')
